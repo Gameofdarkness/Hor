80eda5bf7974e157262b44d035bb414fb3ccf6ed
Diyar_T <dyartymwry1@gmail.com>
2021-02-25 20:48:55 +0330

    New game horror

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1614273377,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"bg_black.png",
+		"exit-sheet0.png",
+		"horror1-sheet0.png",
+		"horror2-sheet0.png",
+		"horror3-sheet0.png",
+		"horror4-sheet0.png",
+		"horror music - soundimage.org_3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
